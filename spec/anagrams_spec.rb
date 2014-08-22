c01841a76cc1d81ec47f1f5020a3e77451efe149
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 10:44:01 -0400

    update sytax for new rspec

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe 'Anagram' do 
   it 'should detect no matches' do
     detector = Anagram.new('diaper')
-    detector.match(%w(hello world zombies pants)).should eq([])
+    expect(detector.match(%w(hello world zombies pants))).to eq([])
   end
 
   it 'should detect a simple anagram' do
     detector = Anagram.new('ba')
     anagrams = detector.match(['ab', 'abc', 'bac'])
-    anagrams.should eq(['ab'])
+    expect(anagrams).to eq(['ab'])
   end
 
   it 'should detect an anagram' do
     detector = Anagram.new('listen')
     anagrams = detector.match %w(enlists google inlets banana)
-    anagrams.should eq(['inlets'])
+    expect(anagrams).to eq(['inlets'])
   end
 
   it 'should detect multiple anagrams' do
     detector = Anagram.new('allergy')
     anagrams = detector.match %w(gallery ballerina regally clergy largely leading)
-    anagrams.should eq(['gallery', 'regally', 'largely'])
+    expect(anagrams).to eq(['gallery', 'regally', 'largely'])
   end
 end
