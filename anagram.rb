43d386c4f132faf20ef5f76b8d5da8d9c1f761c3
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 10:43:49 -0400

    add solution

--- anagram.rb
+++ anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
